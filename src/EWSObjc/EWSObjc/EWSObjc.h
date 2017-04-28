feaeefb62a3732a47cacc01431eee6f2e7c7bed8
gaoyq <gaoyq@35.cn>
2017-04-28 16:53:13 +0800

    Init Project

---
+++ src/EWSObjc/EWSObjc/EWSObjc.h
@@ -0,0 +1,19 @@
+//
+//  EWSObjc.h
+//  EWSObjc
+//
+//  Created by gaoyq on 28/04/2017.
+//  Copyright © 2017 QiaQia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EWSObjc.
+FOUNDATION_EXPORT double EWSObjcVersionNumber;
+
+//! Project version string for EWSObjc.
+FOUNDATION_EXPORT const unsigned char EWSObjcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EWSObjc/PublicHeader.h>
+
+
